ee1052fd2d127ef194b726602ce4b537cb8f472f
hyukjin.jang <hyukjin.jang@navercorp.com>
2026-05-11 23:31:25 +0900

    Week 1 - LC 49 Group Anagrams (janghyukjin)

---
+++ solutions/janghyukjin/week-01/lc-49-group-anagrams.py
@@ -0,0 +1,11 @@
+from collections import Counter, defaultdict
+from typing import List
+
+
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        arr = defaultdict(list)
+        for s in strs:
+            key = tuple(sorted(s))
+            arr[key].append(s)
+        return list(arr.values())
